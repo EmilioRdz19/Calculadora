b656d9e1a4f6905b265e0bd24f803ec290c924ec
Emilio Rodriguez <jorge.rodriguezv@becarios.infotec.mx>
2022-06-18 17:16:54 -0500

    we create the calculator buttons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const createDigits = () => {
+      const digits = [];
+
+      for (let i = 1; i < 10; i++) {
+        digits.push(
+          <botton key={i}>{i}</botton>
+        )
+      }
+
+      return digits;
+    }
   return (
     <div className="App">
-      Hello World
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <buttom>/</buttom>
+          <buttom>*</buttom>
+          <buttom>+</buttom>
+          <buttom>-</buttom>
+
+          <buttom>DEL</buttom>
+          </div>
+
+          <div className="digits">
+          { createDigits() }
+          <buttom>0</buttom>
+          <buttom>.</buttom>
+          <buttom>=</buttom>
+          </div>
+
+      </div>
     </div>
   );
 }
